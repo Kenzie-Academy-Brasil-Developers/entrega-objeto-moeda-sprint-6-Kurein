74a5486fb5e727b68e986c5a09ee52a3240af4fa
Kaueh <kaueh1234@gmail.com>
2021-08-18 19:10:25 -0300

    Entrega concluida

---
+++ coinObject/coinObject.js
@@ -0,0 +1,59 @@
+const coin = {
+    state : 0,
+
+    flip: function() {
+        this.state = Math.round(Math. random())
+    },
+
+    toString: function() {
+        if (this.state === 0){
+            return "Heads"
+        }
+        if (this.state === 1){
+            return "Tails"
+        }
+    },
+
+    toHTML: function() {
+        const image = document.createElement("img")
+        if (this.state === 0){
+            image.src = "./images/Heads.jpg"
+            image.alt = "Heads"
+        }
+        if (this.state === 1){
+            image.src = "./images/Tails.jpg"
+            image.alt = "Tails"
+        }
+
+        return image
+    }
+}
+
+function display20Flips() {
+    const results = []
+    const paragraph = document.createElement("p")
+
+    for(let i=0; i<20; i++){
+        coin.flip()
+        results.push(coin.toString())
+        paragraph.innerText = results[i]
+        let paragraphClone = paragraph.cloneNode(true)
+        document.body.appendChild(paragraphClone)
+    }
+
+    return results
+}
+
+function display20Images() {
+    const results = []
+
+    for(let j=0; j<20; j++){
+        coin.flip()
+        results.push(coin.toHTML())
+        let image = coin.toHTML()
+        let imageClone = image.cloneNode()
+        document.body.appendChild(imageClone)
+    }
+
+    return results
+}
